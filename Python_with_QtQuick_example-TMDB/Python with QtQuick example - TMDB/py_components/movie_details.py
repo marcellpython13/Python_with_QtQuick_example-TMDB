0af3ad2e314ee00364f5400c27217d91290876dc
marcellpython13 <131479035+marcellpython13@users.noreply.github.com>
2023-12-10 14:35:13 +0100

    Add files via upload

---
+++ Python_with_QtQuick_example-TMDB/Python with QtQuick example - TMDB/py_components/movie_details.py
@@ -0,0 +1,85 @@
+import tmdbsimple as tmdb
+from datetime import datetime, timedelta
+from py_components.resources import get_image_from_url
+from PySide6.QtCore import QObject, Slot, Signal, Property, QUrl
+
+class MovieDetails(QObject):
+    movie_changed = Signal()
+
+    def __init__(self):
+        super().__init__()
+        self._movie_data = None
+
+    @Slot(int)
+    def set_movie(self, movie_id):
+        movie = tmdb.Movies(movie_id)
+        self._movie_data = movie.info()
+        self.movie_changed.emit()
+
+    def _get_title(self):
+        if not self._movie_data:
+            return ""
+        
+        title = self._movie_data.get("title")
+        year = datetime.strptime(self._movie_data.get("release_date"), "%Y-%m-%d").year
+        return f"{title} ({year})"
+
+    def _get_overview(self):
+        return self._movie_data.get("overview") if self._movie_data else ""
+    
+    def _get_tagline(self):
+        return self._movie_data.get("tagline") if self._movie_data else ""
+    
+    def _get_genres(self):
+        if not self._movie_data:
+            return ""
+        
+        genres = [i["name"] for i in self._movie_data.get("genres")]
+        return ", ".join(genres) if self._movie_data else ""
+    
+    def _get_poster(self):
+        return get_image_from_url(self._movie_data.get("poster_path")) if self._movie_data else ""
+    
+    def _get_release_date(self):
+        if not self._movie_data:
+            return ""
+        
+        release_date = datetime.strptime(self._movie_data.get("release_date"), "%Y-%m-%d")
+        return release_date.strftime("%Y %B %d")
+    
+    def _get_runtime(self):
+        if not self._movie_data:
+            return ""
+        
+        t_delta = timedelta(seconds=self._movie_data.get("runtime"))
+        d, h, m = str(t_delta).split(":")
+
+        return f"{int(h)}h {int(m)}m"
+    
+    def _get_vote_average(self):
+        if not self._movie_data:
+            return 0
+        
+        return self._movie_data.get("vote_average") * 10
+    
+    def _get_backdrop(self):
+        if not self._movie_data:
+            return ""
+        
+        backdrop_url = self._movie_data.get("backdrop_path")
+
+        if not backdrop_url:
+            return ""
+        
+        backdrop_server_path = f"https://image.tmdb.org/t/p/w1920_and_h800_multi_faces"
+        return get_image_from_url(f"{backdrop_server_path}{backdrop_url}")
+
+    title = Property(str, _get_title, notify=movie_changed)
+    overview = Property(str, _get_overview, notify=movie_changed)
+    tagline = Property(str, _get_tagline, notify=movie_changed)
+    genres = Property(str, _get_genres, notify=movie_changed)
+    poster = Property(QUrl, _get_poster, notify=movie_changed)
+    release_date = Property(str, _get_release_date, notify=movie_changed)
+    runtime = Property(str, _get_runtime, notify=movie_changed)
+    vote_average = Property(int, _get_vote_average, notify=movie_changed)
+    backdrop = Property(QUrl, _get_backdrop, notify=movie_changed)
---
+++ Python_with_QtQuick_example-TMDB/Python with QtQuick example - TMDB/qml_components/custom_components/PopularityProgress.qml
@@ -0,0 +1,55 @@
+import QtQuick
+
+Rectangle{
+    id: root
+    width: 50
+    height: 50
+    radius: width
+    color: "black"
+    anchors.bottom: parent.bottom
+    anchors.bottomMargin: - 25
+    x: 10
+
+    property int popularity
+    
+    onPopularityChanged: draw_canvas.requestPaint()
+
+    Canvas{
+        id: draw_canvas
+        width: parent.width
+        height: parent.height
+
+        onPaint: {
+            var ctx = getContext("2d")
+            var radiant = root.popularity * 0.062831853071796
+            var centerX = width / 2
+            var centerY = height / 2
+            var radius = width / 2
+
+            ctx.reset()
+            ctx.beginPath()
+            ctx.fillStyle = root.popularity > 70 ? "lightgreen" : "yellow"
+            ctx.arc(centerX, centerY, radius, 0, radiant, false)
+            ctx.lineTo(centerX, centerY)
+            ctx.fill()
+        }
+
+        rotation: - 90
+    }
+
+    Rectangle{
+        color: root.color
+        width: root.width - 10
+        height: width
+        radius: width
+        anchors.centerIn: parent
+    }
+
+    Text{
+        anchors.centerIn: parent
+        text: root.popularity
+        font.bold: true
+        font.pixelSize: 24
+        color: "white"
+    }
+}
